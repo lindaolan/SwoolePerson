66d66a0f6948d1074b4d6fc1cbfea4261fe3b25d
lindaolan <34884083+lindaolan@users.noreply.github.com>
2018-08-30 09:07:53 +0800

    Create Controller.php

---
+++ src/Server/Controller/Controller.php
@@ -0,0 +1,14 @@
+<?php
+namespace Server\Controller;
+class Person
+{
+    private $_name = null;
+    public function getPersonName()
+    {
+        return $this->_name;
+    }
+    public function setPersonName($name)
+    {
+        $this->_name = $name;
+    }
+}
